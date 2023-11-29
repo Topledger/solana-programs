2041bd169c63b08ec5ddc1fa1682a522047bc87c
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2023-11-29 09:48:06 +0530

    Add Parsing for Inner Instructions

--- metaplex/token-metadata/src/lib.rs
+++ metaplex/token-metadata/src/lib.rs
@@ -32,7 +32,6 @@ fn map_block(block: Block) -> Result<Output, substreams::errors::Error> {
 
             for (idx, inst) in msg.instructions.into_iter().enumerate() {
                 let program = &accounts[inst.program_id_index as usize];
-                let tx_id = bs58::encode(&transaction.signatures[0]).into_string();
                 let parsed_arg_data = get_arg(program, inst.data);
                 if parsed_arg_data.is_some() {
                     let mut tokenMetadataMeta: TokenMetadataMeta = TokenMetadataMeta::default();
@@ -46,13 +45,48 @@ fn map_block(block: Block) -> Result<Output, substreams::errors::Error> {
                     tokenMetadataMeta.block_date = convert_to_date(timestamp);
                     tokenMetadataMeta.block_time = timestamp;
                     tokenMetadataMeta.block_slot = slot;
-                    tokenMetadataMeta.tx_id = tx_id;
+                    tokenMetadataMeta.tx_id =
+                        bs58::encode(&transaction.signatures[0]).into_string();
                     tokenMetadataMeta.dapp = constants::TOKEN_METADATA_PROGRAM_ADDRESS.to_string();
                     tokenMetadataMeta.instruction_index = idx as u32;
                     tokenMetadataMeta.is_inner_instruction = false;
                     tokenMetadataMeta.inner_instruction_index = 0;
                     data.push(tokenMetadataMeta);
                 }
+
+                meta.inner_instructions
+                    .iter()
+                    .filter(|inner_instruction| inner_instruction.index == idx as u32)
+                    .for_each(|inner_instruction| {
+                        inner_instruction.instructions.iter().enumerate().for_each(
+                            |(inner_idx, inner_inst)| {
+                                let program = &accounts[inner_inst.program_id_index as usize];
+                                let parsed_arg_data = get_arg(program, inner_inst.data.clone());
+                                if parsed_arg_data.is_some() {
+                                    let mut tokenMetadataMeta: TokenMetadataMeta =
+                                        TokenMetadataMeta::default();
+                                    tokenMetadataMeta.arg = parsed_arg_data.unwrap();
+                                    tokenMetadataMeta.input_accounts = prepare_input_accounts(
+                                        tokenMetadataMeta.arg.instruction_type.clone(),
+                                        &inner_inst.accounts,
+                                        &accounts,
+                                    );
+
+                                    tokenMetadataMeta.block_date = convert_to_date(timestamp);
+                                    tokenMetadataMeta.block_time = timestamp;
+                                    tokenMetadataMeta.block_slot = slot;
+                                    tokenMetadataMeta.tx_id =
+                                        bs58::encode(&transaction.signatures[0]).into_string();
+                                    tokenMetadataMeta.dapp =
+                                        constants::TOKEN_METADATA_PROGRAM_ADDRESS.to_string();
+                                    tokenMetadataMeta.instruction_index = idx as u32;
+                                    tokenMetadataMeta.is_inner_instruction = true;
+                                    tokenMetadataMeta.inner_instruction_index = inner_idx as u32;
+                                    data.push(tokenMetadataMeta);
+                                }
+                            },
+                        );
+                    });
             }
         }
     }
