b49256f07f98e01395819aa76dfa4a575125abad
dkdocs <deepak@topledger.xyz>
2023-12-19 01:18:51 +0530

    adds program transactions substream

---
+++ filter-program-transactions/src/pb/mod.rs
@@ -0,0 +1,12 @@
+// @generated
+pub mod sf {
+    pub mod solana {
+        pub mod transactions {
+            // @@protoc_insertion_point(attribute:sf.solana.transactions.v1)
+            pub mod v1 {
+                include!("sf.solana.transactions.v1.rs");
+                // @@protoc_insertion_point(sf.solana.transactions.v1)
+            }
+        }
+    }
+}
