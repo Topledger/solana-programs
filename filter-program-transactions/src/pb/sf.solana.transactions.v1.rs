dc07e5479f46944dc4046c04d2e586961fda2d19
dkdocs <deepak@topledger.xyz>
2023-12-23 19:57:39 +0530

    Revert "unnests inner_instructions and adds log_messages to output"
    This reverts commit 79f68e5450203adfcb066ede1aff1452753eaa55.

--- filter-program-transactions/src/pb/sf.solana.transactions.v1.rs
+++ filter-program-transactions/src/pb/sf.solana.transactions.v1.rs
@@ -11,47 +11,41 @@ pub struct TransactionStats {
     #[prost(uint32, required, tag="1")]
     pub block_slot: u32,
     #[prost(string, required, tag="2")]
-    pub program: ::prost::alloc::string::String,
-    #[prost(string, required, tag="3")]
     pub block_date: ::prost::alloc::string::String,
-    #[prost(uint32, required, tag="4")]
+    #[prost(uint32, required, tag="3")]
     pub index: u32,
-    #[prost(uint32, required, tag="5")]
+    #[prost(uint32, required, tag="4")]
     pub required_signatures: u32,
-    #[prost(uint32, required, tag="6")]
+    #[prost(uint32, required, tag="5")]
     pub readonly_signed_accounts: u32,
-    #[prost(uint32, required, tag="7")]
+    #[prost(uint32, required, tag="6")]
     pub readonly_unsigned_accounts: u32,
-    #[prost(string, required, tag="8")]
+    #[prost(string, required, tag="7")]
     pub id: ::prost::alloc::string::String,
-    #[prost(string, repeated, tag="9")]
-    pub account_keys: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(message, repeated, tag="10")]
+    #[prost(bool, required, tag="8")]
+    pub success: bool,
+    #[prost(message, repeated, tag="9")]
     pub instructions: ::prost::alloc::vec::Vec<Instruction>,
-    #[prost(message, repeated, tag="11")]
-    pub inner_instructions: ::prost::alloc::vec::Vec<InnerInstruction>,
-    #[prost(uint64, repeated, packed="false", tag="12")]
+    #[prost(uint64, repeated, packed="false", tag="10")]
     pub pre_balances: ::prost::alloc::vec::Vec<u64>,
-    #[prost(uint64, repeated, packed="false", tag="13")]
+    #[prost(uint64, repeated, packed="false", tag="11")]
     pub post_balances: ::prost::alloc::vec::Vec<u64>,
-    #[prost(message, repeated, tag="14")]
+    #[prost(message, repeated, tag="12")]
     pub pre_token_balances: ::prost::alloc::vec::Vec<TokenBalance>,
-    #[prost(message, repeated, tag="15")]
+    #[prost(message, repeated, tag="13")]
     pub post_token_balances: ::prost::alloc::vec::Vec<TokenBalance>,
-    #[prost(string, repeated, tag="16")]
+    #[prost(string, repeated, tag="14")]
     pub signatures: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(string, required, tag="17")]
+    #[prost(string, required, tag="15")]
     pub signer: ::prost::alloc::string::String,
-    #[prost(string, required, tag="18")]
+    #[prost(string, required, tag="16")]
     pub version: ::prost::alloc::string::String,
-    #[prost(string, repeated, tag="19")]
+    #[prost(string, repeated, tag="17")]
     pub executing_accounts: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(string, repeated, tag="29")]
-    pub log_messages: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(bool, required, tag="30")]
+    #[prost(bool, required, tag="18")]
     pub logs_truncated: bool,
-    #[prost(uint64, required, tag="31")]
-    pub block_time: u64,
+    #[prost(string, required, tag="19")]
+    pub program: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -62,9 +56,11 @@ pub struct Instruction {
     pub account_arguments: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(string, required, tag="3")]
     pub data: ::prost::alloc::string::String,
-    #[prost(string, repeated, tag="4")]
+    #[prost(message, repeated, tag="4")]
+    pub inner_instructions: ::prost::alloc::vec::Vec<InnerInstruction>,
+    #[prost(string, repeated, tag="8")]
     pub program_logs: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(string, repeated, tag="5")]
+    #[prost(string, repeated, tag="9")]
     pub program_data: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
@@ -80,8 +76,6 @@ pub struct InnerInstruction {
     pub program_logs: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(string, repeated, tag="5")]
     pub program_data: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(uint32, required, tag="6")]
-    pub outer_program_index: u32,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
