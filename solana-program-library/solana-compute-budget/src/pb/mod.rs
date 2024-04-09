8d51d4f2bac85a578217d5cb660fcf58d0c21566
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2024-04-09 14:37:37 +0530

    add solana compute budget substream

---
+++ solana-program-library/solana-compute-budget/src/pb/mod.rs
@@ -0,0 +1,16 @@
+// @generated
+pub mod sf {
+    pub mod solana {
+        pub mod programs {
+            pub mod compute {
+                pub mod budget {
+                    // @@protoc_insertion_point(attribute:sf.solana.programs.compute.budget.v1)
+                    pub mod v1 {
+                        include!("sf.solana.programs.compute.budget.v1.rs");
+                        // @@protoc_insertion_point(sf.solana.programs.compute.budget.v1)
+                    }
+                }
+            }
+        }
+    }
+}
