7a0742fdf611215e7630ece294a732fb2a89e63e
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2024-08-08 19:21:10 +0530

    add helium entity-manager substream

---
+++ helium/entity-manager/src/prepare_input_accounts.rs
@@ -0,0 +1,40 @@
+use crate::pb::sf::solana::block_meta::v1::InputAccounts;
+
+pub fn prepare_input_accounts(
+    instruction_type: String,
+    account_indices: &Vec<u8>,
+    accounts: &Vec<String>,
+) -> InputAccounts {
+    let input_accounts = populate_input_accounts(account_indices, accounts);
+
+    let mut result = InputAccounts::default();
+    match instruction_type.as_str() {
+        "InitializeRewardableEntityConfigV0" => {
+            result.payer = get_account_with(&input_accounts, 0);
+            result.sub_dao = get_account_with(&input_accounts, 1);
+            result.authority = get_account_with(&input_accounts, 2);
+            result.rewardable_entity_config = get_account_with(&input_accounts, 3);
+            result.system_program = get_account_with(&input_accounts, 4);
+        }
+        _ => {}
+    }
+
+    return result;
+}
+
+fn get_account_with(accounts: &Vec<String>, index: usize) -> Option<String> {
+    let mut result: Option<String> = None;
+    let account = accounts.get(index);
+    if account.is_some() {
+        result = Some(account.unwrap().to_string());
+    }
+    return result;
+}
+
+fn populate_input_accounts(account_indices: &Vec<u8>, accounts: &Vec<String>) -> Vec<String> {
+    let mut instruction_accounts: Vec<String> = vec![];
+    for (index, &el) in account_indices.iter().enumerate() {
+        instruction_accounts.push(accounts.as_slice()[el as usize].to_string());
+    }
+    return instruction_accounts;
+}
