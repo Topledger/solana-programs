acced04e445f3c70cd1323dd1a3102b95007450e
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2025-01-13 16:03:35 +0530

    update dex-trades substream

--- dex-trades/src/dapps/dapp_LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo.rs
+++ dex-trades/src/dapps/dapp_LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo.rs
@@ -1,6 +1,9 @@
 use crate::trade_instruction::TradeInstruction;
 
 const SWAP_DISCRIMINATOR: u64 = u64::from_le_bytes([248, 198, 158, 145, 225, 117, 135, 200]);
+const SWAP_EXACT_OUT_DISCRIMINATOR: u64 = u64::from_le_bytes([250, 73, 101, 33, 38, 207, 75, 184]);
+const SWAP_WITH_PRICE_IMPACT_DISCRIMINATOR: u64 =
+    u64::from_le_bytes([56, 173, 230, 208, 173, 228, 156, 205]);
 
 pub fn parse_trade_instruction(
     bytes_stream: Vec<u8>,
@@ -17,6 +20,26 @@ pub fn parse_trade_instruction(
             result = Some(TradeInstruction {
                 dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
                 name: String::from("Swap"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP_EXACT_OUT_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapExactOut"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP_WITH_PRICE_IMPACT_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapWithPriceImpact"),
                 amm: input_accounts.get(0).unwrap().to_string(),
                 vault_a: input_accounts.get(2).unwrap().to_string(),
                 vault_b: input_accounts.get(3).unwrap().to_string(),
