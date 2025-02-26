564db41a8724b44250a07f9bcfaf75d8b770c2cc
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2025-02-26 11:39:50 +0530

    add metaplex token-metadata wasm

--- metaplex/token-metadata/src/lib.rs
+++ metaplex/token-metadata/src/lib.rs
@@ -7,108 +7,46 @@ mod instructions;
 mod pb;
 mod prepare_arg;
 mod prepare_input_accounts;
-mod utils;
 
-use pb::sf::solana::block_meta::v1::{Arg, Output, TokenMetadataMeta};
+use pb::sf::solana::block_meta::v1::{Arg, InputAccounts};
 use prepare_arg::prepare_arg;
 use prepare_input_accounts::prepare_input_accounts;
-use substreams::log;
-use substreams_solana::pb::sf::solana::r#type::v1::Block;
-use utils::convert_to_date;
 
-#[substreams::handlers::map]
-fn map_block(block: Block) -> Result<Output, substreams::errors::Error> {
-    let slot = block.slot;
-    let parent_slot = block.parent_slot;
-    let timestamp = block.block_time.as_ref().unwrap().timestamp;
+use serde_wasm_bindgen;
+use std::panic;
+use wasm_bindgen::prelude::*;
 
-    let mut data: Vec<TokenMetadataMeta> = vec![];
+use serde::Serialize;
 
-    for trx in block.transactions_owned() {
-        let accounts = trx.resolved_accounts_as_strings();
-        if let Some(transaction) = trx.transaction {
-            let msg = transaction.message.unwrap();
-            let meta = trx.meta.unwrap();
-
-            for (idx, inst) in msg.instructions.into_iter().enumerate() {
-                let program = &accounts[inst.program_id_index as usize];
-                let tx_id = bs58::encode(&transaction.signatures[0]).into_string();
-                let parsed_arg_data = get_arg(program, inst.data, tx_id.clone());
-                if parsed_arg_data.is_some() {
-                    let mut tokenMetadataMeta: TokenMetadataMeta = TokenMetadataMeta::default();
-                    tokenMetadataMeta.args = parsed_arg_data.unwrap();
-                    tokenMetadataMeta.instruction_type =
-                        tokenMetadataMeta.args.instruction_type.clone();
-                    tokenMetadataMeta.input_accounts = prepare_input_accounts(
-                        tokenMetadataMeta.args.instruction_type.clone(),
-                        &inst.accounts,
-                        &accounts,
-                    );
-
-                    tokenMetadataMeta.block_date = convert_to_date(timestamp);
-                    tokenMetadataMeta.block_time = timestamp;
-                    tokenMetadataMeta.block_slot = slot;
-                    tokenMetadataMeta.tx_id = tx_id.clone();
-                    tokenMetadataMeta.dapp = constants::TOKEN_METADATA_PROGRAM_ADDRESS.to_string();
-                    tokenMetadataMeta.instruction_index = idx as u32;
-                    tokenMetadataMeta.is_inner_instruction = false;
-                    tokenMetadataMeta.inner_instruction_index = 0;
-                    data.push(tokenMetadataMeta);
-                }
-
-                meta.inner_instructions
-                    .iter()
-                    .filter(|inner_instruction| inner_instruction.index == idx as u32)
-                    .for_each(|inner_instruction| {
-                        inner_instruction.instructions.iter().enumerate().for_each(
-                            |(inner_idx, inner_inst)| {
-                                let program = &accounts[inner_inst.program_id_index as usize];
-                                let parsed_arg_data =
-                                    get_arg(program, inner_inst.data.clone(), tx_id.clone());
-                                if parsed_arg_data.is_some() {
-                                    let mut tokenMetadataMeta: TokenMetadataMeta =
-                                        TokenMetadataMeta::default();
-                                    tokenMetadataMeta.args = parsed_arg_data.unwrap();
-                                    tokenMetadataMeta.instruction_type =
-                                        tokenMetadataMeta.args.instruction_type.clone();
-                                    tokenMetadataMeta.input_accounts = prepare_input_accounts(
-                                        tokenMetadataMeta.args.instruction_type.clone(),
-                                        &inner_inst.accounts,
-                                        &accounts,
-                                    );
-
-                                    tokenMetadataMeta.block_date = convert_to_date(timestamp);
-                                    tokenMetadataMeta.block_time = timestamp;
-                                    tokenMetadataMeta.block_slot = slot;
-                                    tokenMetadataMeta.tx_id = tx_id.clone();
-                                    tokenMetadataMeta.dapp =
-                                        constants::TOKEN_METADATA_PROGRAM_ADDRESS.to_string();
-                                    tokenMetadataMeta.instruction_index = idx as u32;
-                                    tokenMetadataMeta.is_inner_instruction = true;
-                                    tokenMetadataMeta.inner_instruction_index = inner_idx as u32;
-                                    data.push(tokenMetadataMeta);
-                                }
-                            },
-                        );
-                    });
-            }
-        }
-    }
-
-    log::info!("{:#?}", slot);
-    Ok(Output { data })
+#[wasm_bindgen(start)]
+pub fn run() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
-fn get_arg(program: &String, instruction_data: Vec<u8>, tx_id: String) -> Option<Arg> {
-    let mut result = None;
+#[derive(Serialize)]
+struct Instruction {
+    joinKey: String,
+    arg: Arg,
+    inputAccounts: InputAccounts,
+}
 
-    if program
-        .to_string()
-        .ne(constants::TOKEN_METADATA_PROGRAM_ADDRESS)
-    {
-        return result;
-    } else {
-        result = Some(prepare_arg(instruction_data, tx_id));
-        return result;
+#[wasm_bindgen]
+pub fn parse(join_key: &str, base58_str: &str, accounts_js: JsValue) -> JsValue {
+    let decoded_bytes: Vec<u8> = bs58::decode(base58_str).into_vec().unwrap();
+    let accounts: Vec<String> = accounts_js.into_serde().unwrap();
+
+    let mut instruction: Instruction = parse_instruction(decoded_bytes, &accounts);
+
+    instruction.joinKey = join_key.to_string();
+    serde_wasm_bindgen::to_value(&instruction).unwrap()
+}
+
+fn parse_instruction(instruction_data: Vec<u8>, accounts: &Vec<String>) -> Instruction {
+    let arg = prepare_arg(instruction_data);
+    let input_accounts = prepare_input_accounts(arg.instruction_type.clone(), accounts);
+    Instruction {
+        joinKey: "".to_string(),
+        arg: arg,
+        inputAccounts: input_accounts,
     }
 }
