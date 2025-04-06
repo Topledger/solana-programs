befaa6bcd4faefeac2f2642c6eec09db546e9ed0
whitewalker007 <nitinkrshukla@gmail.com>
2025-04-07 01:38:48 +0530

    adds raydium cpmm instruction parser

---
+++ raydium/cpmm/src/pb/mod.rs
@@ -0,0 +1,35 @@
+// @generated
+pub mod sf {
+    pub mod solana {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
+            pub mod v1 {
+                include!("sf.solana.type.v1.rs");
+                // @@protoc_insertion_point(sf.solana.type.v1)
+            }
+        }
+        pub mod raydium_cpmm {
+            // @@protoc_insertion_point(attribute:sf.solana.raydium_cpmm.v1)
+            pub mod v1 {
+                include!("sf.solana.raydium_cpmm.v1.rs");
+                // @@protoc_insertion_point(sf.solana.raydium_cpmm.v1)
+            }
+        }
+    }
+}
+pub mod sol {
+    pub mod instructions {
+        // @@protoc_insertion_point(attribute:sol.instructions.v1)
+        pub mod v1 {
+            include!("sol.instructions.v1.rs");
+            // @@protoc_insertion_point(sol.instructions.v1)
+        }
+    }
+    pub mod transactions {
+        // @@protoc_insertion_point(attribute:sol.transactions.v1)
+        pub mod v1 {
+            include!("sol.transactions.v1.rs");
+            // @@protoc_insertion_point(sol.transactions.v1)
+        }
+    }
+}
