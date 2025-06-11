c0471efda5f35701c3f4900ba92f5548a97ecef8
whitewalker007 <nitinkrshukla@gmail.com>
2025-06-11 23:59:46 +0530

    added orbic swap2

--- dex-trades/src/dapps/dapp_obriQD1zbpyLz95G5n7nJe6a4DPjpFwa5XYPoNm113y.rs
+++ dex-trades/src/dapps/dapp_obriQD1zbpyLz95G5n7nJe6a4DPjpFwa5XYPoNm113y.rs
@@ -1,6 +1,7 @@
 use crate::trade_instruction::TradeInstruction;
 
 const SWAP_DISCRIMINATOR: u64 = u64::from_le_bytes([248, 198, 158, 145, 225, 117, 135, 200]);
+const SWAP2_DISCRIMINATOR: u64 = u64::from_le_bytes([65, 75, 63, 76, 235, 91, 91, 136]);
 
 pub fn parse_trade_instruction(
     bytes_stream: Vec<u8>,
@@ -17,6 +18,16 @@ pub fn parse_trade_instruction(
             result = Some(TradeInstruction {
                 dapp_address: String::from("obriQD1zbpyLz95G5n7nJe6a4DPjpFwa5XYPoNm113y"),
                 name: String::from("Swap"),
+                amm: accounts.get(0).unwrap().to_string(),
+                vault_a: accounts.get(3).unwrap().to_string(),
+                vault_b: accounts.get(4).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP2_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("obriQD1zbpyLz95G5n7nJe6a4DPjpFwa5XYPoNm113y"),
+                name: String::from("Swap2"),
                 amm: accounts.get(0).unwrap().to_string(),
                 vault_a: accounts.get(3).unwrap().to_string(),
                 vault_b: accounts.get(4).unwrap().to_string(),
