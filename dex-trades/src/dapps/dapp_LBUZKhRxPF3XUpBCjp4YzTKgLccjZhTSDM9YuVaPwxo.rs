42e1e4e9acdd712dfb09ac908bba731c6f8f6bca
whitewalker007 <nitinkrshukla@gmail.com>
2025-07-04 12:53:26 +0530

    adds swap2 in meteora dlmm

--- dex-trades/src/dapps/dapp_LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo.rs
+++ dex-trades/src/dapps/dapp_LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo.rs
@@ -5,6 +5,12 @@ const SWAP_EXACT_OUT_DISCRIMINATOR: u64 = u64::from_le_bytes([250, 73, 101, 33, 
 const SWAP_WITH_PRICE_IMPACT_DISCRIMINATOR: u64 =
     u64::from_le_bytes([56, 173, 230, 208, 173, 228, 156, 205]);
 
+// New V2 instruction discriminators
+const SWAP2_DISCRIMINATOR: u64 = u64::from_le_bytes([65, 75, 63, 76, 235, 91, 91, 136]);
+const SWAP_EXACT_OUT2_DISCRIMINATOR: u64 = u64::from_le_bytes([43, 215, 247, 132, 137, 60, 243, 81]);
+const SWAP_WITH_PRICE_IMPACT2_DISCRIMINATOR: u64 =
+    u64::from_le_bytes([74, 98, 192, 214, 177, 51, 75, 51]);
+
 pub fn parse_trade_instruction(
     bytes_stream: Vec<u8>,
     input_accounts: Vec<String>,
@@ -40,6 +46,36 @@ pub fn parse_trade_instruction(
             result = Some(TradeInstruction {
                 dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
                 name: String::from("SwapWithPriceImpact"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP2_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("Swap2"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP_EXACT_OUT2_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapExactOut2"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        }
+        SWAP_WITH_PRICE_IMPACT2_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapWithPriceImpact2"),
                 amm: input_accounts.get(0).unwrap().to_string(),
                 vault_a: input_accounts.get(2).unwrap().to_string(),
                 vault_b: input_accounts.get(3).unwrap().to_string(),
